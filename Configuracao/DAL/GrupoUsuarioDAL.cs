01c57216397f4e5e42451e140320508e3336c5ff
Claudio Clay <claudio_junior05@hotmail.com>
2023-03-14 16:50:59 -0300

    Criação CRUD
    Create (Criação)
    Read (Leitura)
    Update (Atualização)
    Delete (Exclusão)

---
+++ Configuracao/DAL/GrupoUsuarioDAL.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class GrupoUsuarioDAL
+    {
+        public void Inserir(GrupoUsuario _grupousuario)
+        {
+
+        }
+
+        public List<GrupoUsuario> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+        public List<GrupoUsuario> BuscarPorNomeGrupo(string _nomegrupo)
+        {
+            throw new NotImplementedException();
+        }
+        public List<GrupoUsuario> BuscarPorId(int _id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar(GrupoUsuario _grupousuario)
+        {
+
+        }
+
+        public void Excluir(int _id)
+        {
+
+        }
+    }
+}
