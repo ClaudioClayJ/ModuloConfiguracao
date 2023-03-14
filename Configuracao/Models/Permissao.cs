978962749cf911845d294bf3c21e910a4ebe2fb2
Claudio Clay <claudio_junior05@hotmail.com>
2023-03-14 16:14:05 -0300

    Mapeamento das Tabelas da Models
    Criação das classes das Tabelas na Models

---
+++ Configuracao/Models/Permissao.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Models
+{
+    public class Permissao
+    {
+        public int Id { get; set; }
+        public string Descrissao { get; set; }
+
+        public List<GrupoUsuario> Grupos { get; set; }
+    }
+}
